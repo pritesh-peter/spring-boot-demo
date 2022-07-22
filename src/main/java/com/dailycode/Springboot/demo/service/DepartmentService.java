21759894388ae850cfe482c7001611682082bb0c
pritesh-peter <pritesh.peter@gmail.com>
2022-07-22 22:34:15 +0545

    my commit

---
+++ src/main/java/com/dailycode/Springboot/demo/service/DepartmentService.java
@@ -0,0 +1,17 @@
+package com.dailycode.Springboot.demo.service;
+
+import java.util.List;
+
+import com.dailycode.Springboot.demo.entity.Department;
+
+public interface DepartmentService {
+
+	public Department saveDepartment(Department department);
+
+	public List<Department> fetchDepartmentList();
+
+	public Department fetchDepartmentById(Long departmentId);
+
+	public void deleteDepartmentById(Long departmentId);
+
+}
---
+++ src/main/java/com/dailycode/Springboot/demo/service/DepartmentServiceImpl.java
@@ -0,0 +1,37 @@
+package com.dailycode.Springboot.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dailycode.Springboot.demo.entity.Department;
+import com.dailycode.Springboot.demo.repository.DepartmentRepository;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService{
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+
+	@Override
+	public Department saveDepartment(Department department) {
+		return departmentRepository.save(department);
+	}
+
+	@Override
+	public List<Department> fetchDepartmentList() {
+		return departmentRepository.findAll();
+	}
+
+	@Override
+	public Department fetchDepartmentById(Long departmentId) {
+		return departmentRepository.findById(departmentId).get();
+	}
+
+	@Override
+	public void deleteDepartmentById(Long departmentId) {
+		departmentRepository.deleteById(departmentId);
+	}
+
+}
