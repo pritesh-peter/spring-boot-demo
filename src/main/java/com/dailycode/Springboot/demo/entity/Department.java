df345f6282ff60a97ab67c056bc3d520e626bf17
pritesh-peter <pritesh.peter@gmail.com>
2022-07-25 12:12:52 +0545

    implemented lombok successfully

--- src/main/java/com/dailycode/Springboot/demo/entity/Department.java
+++ src/main/java/com/dailycode/Springboot/demo/entity/Department.java
@@ -6,9 +6,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Entity
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Department {
 	
 	@Id
@@ -19,47 +27,5 @@ public class Department {
 	private String departmentName;
 	private String departmentAddress;
 	private String departmentCode;
-	public Long getDepartmentId() {
-		return departmentId;
-	}
-	public void setDepartmentId(Long departmentId) {
-		this.departmentId = departmentId;
-	}
-	public String getDepartmentName() {
-		return departmentName;
-	}
-	public void setDepartmentName(String departmentName) {
-		this.departmentName = departmentName;
-	}
-	public String getDepartmentAddress() {
-		return departmentAddress;
-	}
-	public void setDepartmentAddress(String departmentAddress) {
-		this.departmentAddress = departmentAddress;
-	}
-	public String getDepartmentCode() {
-		return departmentCode;
-	}
-	public void setDepartmentCode(String departmentCode) {
-		this.departmentCode = departmentCode;
-	}
-	public Department(Long departmentId, @NotBlank(message = "Please add Department Name") String departmentName,
-			String departmentAddress, String departmentCode) {
-		super();
-		this.departmentId = departmentId;
-		this.departmentName = departmentName;
-		this.departmentAddress = departmentAddress;
-		this.departmentCode = departmentCode;
-	}
-	public Department() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	@Override
-	public String toString() {
-		return "Department [departmentId=" + departmentId + ", departmentName=" + departmentName
-				+ ", departmentAddress=" + departmentAddress + ", departmentCode=" + departmentCode + "]";
-	}
-	
-	
+		
 }
--- src/main/java/com/dailycode/Springboot/demo/entity/ErrorMessage.java
+++ src/main/java/com/dailycode/Springboot/demo/entity/ErrorMessage.java
@@ -2,35 +2,18 @@ package com.dailycode.Springboot.demo.entity;
 
 import org.springframework.http.HttpStatus;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorMessage {
 
 	private HttpStatus status;
 	private String message;
-	public HttpStatus getStatus() {
-		return status;
-	}
-	public void setStatus(HttpStatus status) {
-		this.status = status;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public ErrorMessage(HttpStatus status, String message) {
-		super();
-		this.status = status;
-		this.message = message;
-	}
-	public ErrorMessage() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	@Override
-	public String toString() {
-		return "ErrorMessage [status=" + status + ", message=" + message + "]";
-	}
-	
 	
 }
